58a26d14c897cc6be71a2c7008f2f2f7477b7b75
Nguyen Quang Anh <anhquangn2611@gmail.com>
2026-04-14 08:53:48 +0700

    feat: doi dinh dang Serial sang JSON

--- firmware/LM35_TempReader/LM35_TempReader.ino
+++ firmware/LM35_TempReader/LM35_TempReader.ino
@@ -1,25 +1,24 @@
-const int sensor1Pin = A0;
-const int sensor2Pin = A1;
-int adcValues[2]; // Mảng chứa 2 giá trị ADC
-float temp[2];    // Mảng chứa 2 giá trị nhiệt độ
+const int sensorPin1 = A0; 
+const int sensorPin2 = A1; 
 
-void setup() {
+void setup() { 
   Serial.begin(9600);
 }
 
 void loop() {
-  // Đọc giá trị ADC từ 2 kênh A0 và A1
-  adcValues[0] = analogRead(sensor1Pin);
-  adcValues[1] = analogRead(sensor2Pin);
+  // Đọc dữ liệu
+  int read1 = analogRead(sensorPin1);
+  float temp1 = (read1 * 5.0 / 1024.0) * 100.0;
 
-  // Chuyển đổi sang độ C (giả sử nguồn 5V, LM35 tăng 10mV/độ)
-  temp[0] = (adcValues[0] * 5.0 * 100.0) / 1024.0;
-  temp[1] = (adcValues[1] * 5.0 * 100.0) / 1024.0;
-
-  // Gửi dữ liệu định dạng CSV (nhiệt độ 1, nhiệt độ 2)
-  Serial.print(temp[0]);
-  Serial.print(",");
-  Serial.println(temp[1]);
-
-  delay(1000); // Đọc mỗi giây 1 lần
+  int read2 = analogRead(sensorPin2);
+  float temp2 = (read2 * 5.0 / 1024.0) * 100.0;
+  
+  // In ra định dạng JSON: {"temp1": 25.5, "temp2": 26.0}
+  Serial.print("{\"temp1\": ");
+  Serial.print(temp1);
+  Serial.print(", \"temp2\": ");
+  Serial.print(temp2);
+  Serial.println("}");
+  
+  delay(2000); 
 }
